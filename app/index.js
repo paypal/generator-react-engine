2e883bef2e218f7eaafd33678d6066c199a92fff
vhwang <vhwang@ebay.com>
2015-05-27 18:53:28 -0700

    generate express or kraken sample

--- app/index.js
+++ app/index.js
@@ -3,35 +3,49 @@ var generators = require('yeoman-generator');
 module.exports = generators.Base.extend({
     prompting: function () {
         var done = this.async();
-        this.prompt({
-            type    : 'input',
-            name    : 'name',
-            message : 'Your project name',
-            default : this.appname // Default to current folder name
-        }, function (answers) {
+        this.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Your project name',
+                default: this.appname // Default to current folder name
+            },
+            {
+                type: 'list',
+                name: 'framework',
+                message: 'Framework',
+                choices: [
+                    {
+                        name: 'express',
+                        value: 'express'
+                    },
+                    {
+                        name: 'kraken',
+                        value: 'kraken'
+                    }
+                ]
+            }
+        ], function (answers) {
             this.appName = answers.name;
+            this.framework = answers.framework;
             done();
         }.bind(this));
     },
     writing: function() {
         var appRoot = this.appRoot = this.destinationPath(this.appName);
-        var files = [
-            'package.json',
-            'index.js',
-            'public/index.js',
-            'public/views/index.jsx',
-            'public/views/layout.jsx'
-        ];
-        var that = this;
+        var package;
 
         this.mkdir(appRoot);
         process.chdir(appRoot);
 
-        files.forEach(function(file) {
-            that.fs.copy(
-                that.templatePath(file),
-                that.destinationPath(file)
-            );
-        });
+        package = this.fs.readJSON(this.templatePath('common/' + this.framework + '/package.json'));
+        package.name = this.appName;
+
+        this.fs.writeJSON(this.destinationPath('package.json'), package);
+
+        this.fs.copy(
+            this.templatePath(this.framework),
+            this.destinationPath()
+        );
     }
 });
