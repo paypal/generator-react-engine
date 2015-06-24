2782074cb04113ba1be15afc84f8620bcdf1ea58
vhwang <vhwang@ebay.com>
2015-06-24 11:09:03 -0700

    use this.installDependencies instead of making two calls to this.npmInstall

--- app/index.js
+++ app/index.js
@@ -1,12 +1,19 @@
+/*-------------------------------------------------------------------------------------------------------------------*\
+ |  Copyright (C) 2015 PayPal                                                                                          |
+ |                                                                                                                     |
+ |  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance     |
+ |  with the License.                                                                                                  |
+ |                                                                                                                     |
+ |  You may obtain a copy of the License at                                                                            |
+ |                                                                                                                     |
+ |       http://www.apache.org/licenses/LICENSE-2.0                                                                    |
+ |                                                                                                                     |
+ |  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed   |
+ |  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for  |
+ |  the specific language governing permissions and limitations under the License.                                     |
+ \*-------------------------------------------------------------------------------------------------------------------*/
+
 var generators = require('yeoman-generator');
-
-function dependencyObjectToArray(dependency) {
-    var dependencies = [];
-
-    Object.keys(dependency).forEach(function(key) {
-        dependencies.push(key + '@' + dependency[key]);
-    });
-}
 
 module.exports = generators.Base.extend({
     prompting: function () {
@@ -80,15 +87,7 @@ module.exports = generators.Base.extend({
         var dependencies;
 
         if (this.shouldRunInstall) {
-            if (this.package.dependencies) {
-                dependencies = dependencyObjectToArray(this.package.dependencies);
-                this.npmInstall(dependencies);
-            }
-
-            if (this.package.devDependencies) {
-                dependencies = dependencyObjectToArray(this.package.devDependencies);
-                this.npmInstall(dependencies);
-            }
+            this.installDependencies({npm: true, bower: false});
         }
     },
     end: function() {
